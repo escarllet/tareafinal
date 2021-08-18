5a1d6ce29e5ff1d5c258fe89cb716214c86b5d55
escarllet <65832957+escarllet@users.noreply.github.com>
2021-08-18 12:06:53 -0400

    todos los archivos del proyecto
    a excepción de la librería Dompdf

---
+++ functions/bd.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+            'localhost',
+            'root',
+            '',
+            'truck'
+);
+
+//if(isset($conn)){
+
+//echo 'base de datos conectada';
+
+//}
+?>
